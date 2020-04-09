750af6da10a17055ef2fb5e5b7afc796a74762df
Robert Cronin <robert.cronin@uqconnect.edu.au>
2020-04-09 02:37:19 +0000

    Add new configuration files from typescript-demo

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const process = require('process');
+const path = require('path');
+const webpack = require('webpack');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const createStyledComponentsTransformer = require('typescript-plugin-styled-components').default;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const styledComponentsTransformer = createStyledComponentsTransformer();
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: "source-map",
+  devServer: {
+    host: process.env.HOST,
+    port: process.env.PORT,
+    historyApiFallback: true,
+    watchContentBase: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    plugins: [new TsconfigPathsPlugin()]
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          getCustomTransformers: () => ({ before: [styledComponentsTransformer] }),
+        },
+      }
+    ]
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin([
+      'HOST',
+      'PORT'
+    ]),
+    new HtmlWebpackPlugin({
+      template: '!!ejs-compiled-loader!src/index.ejs',
+      inject: 'body',
+      xhtml: true,
+      filename: 'index.html',
+      templateParameters: {
+        title: 'TypeScript Demo',
+        configJs: `
+          window.config = {
+            HOST: '${process.env.HOST}',
+            PORT: '${process.env.PORT}'
+          };
+        `
+      }
+    })
+  ],
+  watchOptions: {
+    ignored: /node_modules/
+  }
+};
