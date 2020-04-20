e6a383692a4a5815baf219f8e51b694e081e9740
Roger Qiu <roger@formbay.com.au>
2020-04-20 20:26:17 +1000

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -2,70 +2,30 @@ const process = require('process');
 const path = require('path');
 const webpack = require('webpack');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-const createStyledComponentsTransformer = require('typescript-plugin-styled-components').default;
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const styledComponentsTransformer = createStyledComponentsTransformer();
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: './src/index.ts',
+  devtool: "source-map",
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'encryptedfs.js',
+    filename: 'index.js',
     library: 'encryptedfs',
     libraryTarget: 'umd',
   },
-  externals: {
-    virtualfs: "virtualfs",
-  },
-  devtool: "source-map",
-  devServer: {
-    host: process.env.HOST,
-    port: process.env.PORT,
-    historyApiFallback: true,
-    watchContentBase: true,
-    contentBase: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
-  },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
     plugins: [new TsconfigPathsPlugin()]
-  },
-  node: {
-    fs: 'empty'
   },
   module: {
     rules: [
       {
         test: /.tsx?$/,
         loader: 'ts-loader',
-        options: {
-          getCustomTransformers: () => ({ before: [styledComponentsTransformer] }),
-        },
       }
     ]
   },
-  plugins: [
-    new webpack.EnvironmentPlugin([
-      'HOST',
-      'PORT'
-    ]),
-    new HtmlWebpackPlugin({
-      // template: '!!ejs-compiled-loader!src/index.ejs',
-      // inject: 'body',
-      // xhtml: true,
-      // filename: 'index.html',
-      // templateParameters: {
-      //   title: 'TypeScript Demo',
-      //   configJs: `
-      //     window.config = {
-      //       HOST: '${process.env.HOST}',
-      //       PORT: '${process.env.PORT}'
-      //     };
-      //   `
-      // }
-    })
-  ],
+  externals: [nodeExternals()],
   watchOptions: {
     ignored: /node_modules/
   }
