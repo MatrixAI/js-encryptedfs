5c1d123f9c39bc21becfbe5b16707919322015e2
Roger Qiu <roger.qiu@polyhack.io>
2021-09-14 17:28:53 +1000

    Removed useless ErrorDBKey* exceptions

--- src/db/errors.ts
+++ src/db/errors.ts
@@ -14,12 +14,6 @@ class ErrorDBDecrypt extends ErrorDB {}
 
 class ErrorDBParse extends ErrorDB {}
 
-class ErrorDBKeyRead extends ErrorDB {}
-
-class ErrorDBKeyWrite extends ErrorDB {}
-
-class ErrorDBKeyParse extends ErrorDB {}
-
 class ErrorDBCommitted extends ErrorDB {}
 
 class ErrorDBNotCommited extends ErrorDB {}
@@ -34,9 +28,6 @@ export {
   ErrorDBLevelPrefix,
   ErrorDBDecrypt,
   ErrorDBParse,
-  ErrorDBKeyRead,
-  ErrorDBKeyWrite,
-  ErrorDBKeyParse,
   ErrorDBCommitted,
   ErrorDBNotCommited,
   ErrorDBRollbacked,
